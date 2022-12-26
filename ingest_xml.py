b8795e595d60cc14aaa98f4e7a730c1cec5d5015
moon-canyon <118559765+moon-canyon@users.noreply.github.com>
2022-12-26 21:00:06 +0100

    it works now

--- ingest_xml.py
+++ ingest_xml.py
@@ -12,5 +12,15 @@ def inxml():
     xml = xmltodict.parse(stream.read())
 
     #iterate through items in the XML object
-    for e in xml:
-        print(e)
+    for e in xml["nmaprun"]["host"]:
+        # if e["status"]["@state"] == "down":
+        #     print("host is down")
+        if e["status"]["@state"] == "up":
+            print(e["address"]["@addr"])
+        
+         
+
+#each host is a dictionary and it has sub dictionaries with it
+#You iterate throught it but if you iterate through the 
+# first host which is ["host"][0] you will end up iterating through a list of dictionary keys like "status" and not other hosts
+#Justa Puma
